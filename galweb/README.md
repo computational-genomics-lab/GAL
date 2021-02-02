57e6ab1a443cf59508aacba711dbb4e9e8f1ec26
arpanda <arijpanda@gmail.com>
2021-02-02 17:05:28 -0600

    web code-initial

--- galweb/README.md
+++ galweb/README.md
@@ -1,5 +1,13 @@
 ## GAL Visualization 
 ```batch
+# Install dependencies
+npm install
+
+# Serve on localhost:5000
 npm run start
+or
 npm run dev
 ```
+
+##  Web design
+Reference design: https://github.com/gurayyarar/AdminBSBMaterialDesign
--- galweb/index.js
+++ galweb/index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('<h1> GAL Visualization</h1>');
 });
 
+// use static path to set the path for the npm modules
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 

b7e2f02353bcd636cdd5b0aad348353623a83662
arpanda <arijpanda@gmail.com>
2023-03-16 17:14:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'numpy>=1.16',
     ],
     classifiers=[
-        'Development Status :: 1 - Alpha',
+        'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
